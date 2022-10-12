976ffee574a61fda1ab9e09fd906a196aa5414c7
LuckTheGamers <thegamers056@gmail.com>
2022-10-12 20:47:49 -0300

    link shorter changes and css + html

--- js/link-shorter.js
+++ js/link-shorter.js
@@ -1,11 +1,7 @@
-let $linkInput = document.querySelector(".link-input");
+let $linkInput = document.querySelector(".link-class");
 let $btn = document.querySelector(".button-send");
-let $linksgenerate = document.querySelector(".links-generate");
-var div = document.createElement("div");
-var p = document.createElement("p");
-var span = document.createElement("span");
-var button = document.createElement("button");
-var a = document.createElement("a");
+let $linksGenerate = document.querySelector(".links-generate");
+let $errorLink = document.querySelector(".error-link")
 
 $btn.addEventListener("click", send);
 
@@ -16,21 +12,43 @@ function get(url){
     return request.responseText;
 }
 
+function createElements(){
+    div = document.createElement("div");
+    p = document.createElement("p");
+    span = document.createElement("span");
+    button = document.createElement("button");
+    a = document.createElement("a");
+    return ;
+}
+
 function createDiv(link){
-    setTimeout(() => {
-        $linksgenerate.appendChild(div);
-        div.setAttribute("class", "generated-div");
-        p.textContent = $linkInput.value;
-        div.appendChild(p);
-        div.appendChild(span);
-        a.textContent = link.result.full_short_link;
-        div.appendChild(a).setAttribute("href", link.result.full_short_link);
-        div.appendChild(button).textContent = "Copy";
-    }, 300);
+    createElements();
+    $linksGenerate.appendChild(div);
+    div.setAttribute("class", "generated-div");
+
+    p.textContent = $linkInput.value;
+    div.appendChild(p);
+    div.appendChild(span);
+    a.textContent = link.result.full_short_link;
+    div.appendChild(a).setAttribute("href", link.result.full_short_link);
+
+    div.appendChild(button).textContent = "Copy";
+    $linkInput.value = "";
 }
 
 function send(){
-    let data = get("https://api.shrtco.de/v2/shorten?url=" + $linkInput.value);
-    let link = JSON.parse(data);
-    createDiv(link);
+    try{
+        if(new URL($linkInput.value)){
+            $linkInput.classList.remove("active")
+            $errorLink.classList.remove("active");
+            var data = get("https://api.shrtco.de/v2/shorten?url=" + $linkInput.value);
+            var link = JSON.parse(data);
+            createDiv(link);
+        }
+    }catch(error){
+        $linkInput.classList.add("active")
+        $errorLink.classList.add("active");
+    }
+    
+    
 }
