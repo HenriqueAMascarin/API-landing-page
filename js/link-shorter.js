3993d582a85fbec5bc70a1a83ff91952bbdebabf
LuckTheGamers <thegamers056@gmail.com>
2022-10-18 19:00:13 -0300

    js complete + working in design

--- js/link-shorter.js
+++ js/link-shorter.js
@@ -2,7 +2,13 @@ let $linkInput = document.querySelector(".link-class");
 let $btn = document.querySelector(".button-send");
 let $linksGenerate = document.querySelector(".links-generate");
 let $errorLink = document.querySelector(".error-link");
+
 $btn.addEventListener("click", send);
+
+document.querySelector(".link-flex").addEventListener("submit", function(e){
+    e.preventDefault();
+    send();
+})
 
 function get(url){
     let request = new XMLHttpRequest();
@@ -11,6 +17,23 @@ function get(url){
     return request.responseText;
 }
 
+function changeBtn(father){
+    father.classList.add("active");
+    father.getElementsByTagName("button")[0].textContent = "Copied!";
+    setTimeout(function(){
+        father.classList.remove("active");
+        father.getElementsByTagName("button")[0].textContent = "Copy";
+    },2000);
+}
+
+$linksGenerate.addEventListener("click", function(e){
+    if(e.target.tagName === "BUTTON" && e.target.parentNode.classList.contains("generated-div")){
+        let father = e.target.parentNode;
+        navigator.clipboard.writeText(father.getElementsByTagName("a")[0].textContent);
+        changeBtn(father);
+    }
+})
+
 function createElements(){
     div = document.createElement("div");
     p = document.createElement("p");
@@ -18,19 +41,6 @@ function createElements(){
     button = document.createElement("button");
     a = document.createElement("a");
 }
-
-$linksGenerate.addEventListener("click", function(e){
-    if(e.target.tagName === "BUTTON" && e.target.parentNode.classList.contains("generated-div")){
-        let father = e.target.parentNode;
-        navigator.clipboard.writeText(father.getElementsByTagName("a")[0].textContent);
-        father.classList.add("active");
-        father.getElementsByTagName("button")[0].textContent = "Copied!";
-        setTimeout(function(){
-            father.classList.remove("active");
-            father.getElementsByTagName("button")[0].textContent = "Copy";
-        },2000);
-    }
-})
 
 function createDiv(link){
     createElements();
@@ -49,22 +59,24 @@ function createDiv(link){
     $linkInput.value = ""; 
 }
 
+function error(){
+    console.clear();
+    data = null;
+    link = null;
+    $linkInput.classList.add("active");
+    $errorLink.classList.add("active");
+}
+
 function send(){
     let data;
     let link;
-    try{
-        data = get("https://api.shrtco.de/v2/shorten?url=" + $linkInput.value);
-        link = JSON.parse(data);
-        if(new URL($linkInput.value) && !!link.error_code === false){
-            $linkInput.classList.remove("active")
-            $errorLink.classList.remove("active");
-            createDiv(link);
-        }
-    }catch(error){
-        $linkInput.classList.add("active")
-        $errorLink.classList.add("active");
-        console.clear();
-        data = null;
-        link = null;
+    data = get("https://api.shrtco.de/v2/shorten?url=" + $linkInput.value);
+    link = JSON.parse(data);
+    if(link.error_code){
+        error();
+        return;
     }
+    $linkInput.classList.remove("active")
+    $errorLink.classList.remove("active");
+    createDiv(link);    
 }
